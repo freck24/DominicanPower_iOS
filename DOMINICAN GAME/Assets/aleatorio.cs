d55b95bd2a049ebeec40986dc818b977e24da1a0
freck24 <83624705+freck24@users.noreply.github.com>
2021-05-05 14:24:44 -0400

    15

---
+++ DOMINICAN GAME/Assets/aleatorio.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class aleatorio : MonoBehaviour
+{
+    
+    public GameObject[] fondos;
+   
+
+    // public SpriteRenderer a;
+   
+    
+   
+    int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = Random.Range(0, fondos.Length-1);
+
+        fondos[i].SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
