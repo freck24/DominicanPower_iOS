d9acd3392979e4d0c6f36c9ce3855245dbd3f39f
freck24 <83624705+freck24@users.noreply.github.com>
2021-05-05 19:06:57 -0400

    25

---
+++ DOMINICAN GAME/Assets/invocando.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invocando : MonoBehaviour
+{
+    public GameObject ene;
+    public Transform[] position;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(corrution());
+    }
+    
+
+    IEnumerator corrution()
+    {
+        int i = 0;
+        while(i<6)
+        
+        {
+            Instantiate(ene, position[i].transform.position, Quaternion.identity);
+            yield return new WaitForSecondsRealtime(0.2f);
+
+            i++;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ DOMINICAN GAME/Assets/jugarnumero.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class jugarnumero : MonoBehaviour
+{
+   
+    // Start is called before the first frame update
+    public Text t;
+    public int numero = 0;
+    public AudioSource a;
+    public AudioClip dinero;
+    public AudioClip facil;
+    public Image yo;
+    void Start()
+    {
+        
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void jugar()
+    {
+        if (PlayerPrefs.GetFloat("dinero", 0) > 99)
+        {
+            PlayerPrefs.SetString("fecha", DateTime.Now.ToString());
+            yo.color = Color.green;
+            PlayerPrefs.SetInt("n" + numero, PlayerPrefs.GetInt("n" + numero, 0) + 100);
+            PlayerPrefs.SetFloat("dinero", PlayerPrefs.GetFloat("dinero", 0) - 100);
+            a.PlayOneShot(dinero);
+        }
+        else
+        {
+            a.PlayOneShot(facil); //quise decir faile
+        }
+       
+    }
+
+    public void ponmenumero()
+    {
+        if (numero <= 9)
+        {
+            t.text = "0" + numero;
+        }
+        else
+        {
+            t.text = "" + numero;
+        }
+       
+    }
+}
