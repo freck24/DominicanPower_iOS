19cc537257492da369728ad5fe87796c7c68c830
freck24 <83624705+freck24@users.noreply.github.com>
2021-05-05 19:04:07 -0400

    24

---
+++ DOMINICAN GAME/Assets/instaciadorsimle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instaciadorsimle : MonoBehaviour
+{
+    public GameObject ene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(it());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator it()
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            Instantiate(ene, transform.position, Quaternion.identity);
+            yield return new WaitForSecondsRealtime(0.5f);
+            i--;
+        }
+    }
+}
---
+++ DOMINICAN GAME/Assets/invocadoraleatorio.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class invocadoraleatorio : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject[] objectos;
+    public int i;
+    void Start()
+    {
+        i = Random.Range(0, objectos.Length);
+        Instantiate(objectos[i], transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
