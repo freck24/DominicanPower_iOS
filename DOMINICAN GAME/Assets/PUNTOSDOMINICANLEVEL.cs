c3b6cda5c15b55108b8325ac4592e4d757ccca0d
freck24 <83624705+freck24@users.noreply.github.com>
2021-05-05 22:39:03 -0400

    34

---
+++ DOMINICAN GAME/Assets/PUNTOSDOMINICANLEVEL.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class PUNTOSDOMINICANLEVEL : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public bool punto = false;
+    //public TextMesh rt;
+    public TextMeshPro rt;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (punto)
+        {
+            rt.text = PlayerPrefs.GetFloat("niveld", 0).ToString();
+        }
+        
+    }
+}
---
+++ DOMINICAN GAME/Assets/Scenes/escenas.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class escenas : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool usuario = false;
+ 
+
+    
+
+    IEnumerator carga()
+    {
+        AsyncOperation async = SceneManager.LoadSceneAsync("segunda 1");
+        Debug.Log("Loading complete");
+        while (usuario == false)
+        {
+            yield return new WaitForSecondsRealtime(1);
+            Debug.Log("esperando");
+           
+           // if(usuario)
+          //  yield return async;
+        }
+
+       
+    }
+
+    public void cambiar()
+    {
+        usuario = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void Start()
+    {
+        StartCoroutine(carga());
+        
+    }
+
+   
+
+
+
+
+
+}
