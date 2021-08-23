2cc416ecff7bd69b8c88dcd776c344f508192215
Aaron Rod <aarone.rodriguez.trujillo@gmail.com>
2021-08-22 22:07:01 -0500

    iOS 14 Ad Tracking Transparency

---
+++ DOMINICAN GAME/Assets/Scripts/iOS!4/IOSATTPostBuild.cs
@@ -0,0 +1,23 @@
+﻿#if UNITY_EDITOR && UNITY_IOS
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+using UnityEditor.iOS.Xcode;
+public class BuildPostProcessInfo
+{
+    [PostProcessBuild]
+    public static void OnPostBuildProcessInfo(BuildTarget target, string path)
+    {
+        if(target == BuildTarget.iOS)
+        {
+            var infoPlistPath = path + "/Info.plist";
+            PlistDocument document = new PlistDocument();
+            document.ReadFromString(File.ReadAllText(infoPlistPath));
+            var elementRoot = document.root;
+            elementRoot.SetString("NSUserTrackingUsageDescription", "This identifier will be used to deliver personalized ads to you.");
+            File.WriteAllText(infoPlistPath, document.WriteToString());
+        }
+    }
+}
+
+#endif
