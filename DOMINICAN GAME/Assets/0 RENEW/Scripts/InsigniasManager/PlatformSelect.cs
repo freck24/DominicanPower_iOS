fa0056bed2c87e7d47e0d8a7b701f8b716484d7b
EliezerYT <46678316+EliezerYT@users.noreply.github.com>
2021-10-12 09:28:09 -0400

    Update PlatformSelect.cs

--- DOMINICAN GAME/Assets/0 RENEW/Scripts/InsigniasManager/PlatformSelect.cs
+++ DOMINICAN GAME/Assets/0 RENEW/Scripts/InsigniasManager/PlatformSelect.cs
@@ -5,8 +5,32 @@ using UnityEngine;
 public class PlatformSelect : MonoBehaviour
 {
     public static PlatformSelect ps;
+    public static PlatformSelect psLocal;
+    public bool statica;
     public bool IsAppleVersion;
 
 
-    void Start() => ps = this;
+    [Header("Links")]
+    public string Link_Android;
+    public string Link_Apple;
+
+    void Start()
+    {
+        if (statica) ps = this;
+        else psLocal = this;
+    }
+
+    public string Get_PlatformLink()
+    {
+        return (PlatformSelect.ps.IsAppleVersion) ? Link_Apple : Link_Android;
+    }
+
+
+    void AbrirLink(string sX)
+    {
+    AbrirLink(PlatformSelect.psLocal.Get_PlatformLink());
+
+    print(sX);
+    }
+
 }
