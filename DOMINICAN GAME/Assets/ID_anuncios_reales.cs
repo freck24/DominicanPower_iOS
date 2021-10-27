270b89aa80010639c726899dc1d88e0c15caf28d
EliezerYT <eliezerterrero275@gmail.com>
2021-10-26 20:21:11 -0400

    Update ID_anuncios_reales.cs

--- DOMINICAN GAME/Assets/ID_anuncios_reales.cs
+++ DOMINICAN GAME/Assets/ID_anuncios_reales.cs
@@ -4,17 +4,59 @@ using UnityEngine;
 
 public class ID_anuncios_reales : MonoBehaviour
 {
+
+    public bool testing;
+    [Header("Datos Plataforma")]
+    public string ID_App;
+    public string ID_VideoReward;
+    public string ID_Intersticial;
+
+
+
+    [Header("Datos Android")]
+    public string Id_Android_APP = "";
     public string Id_Android_Bonificado = "";
     public string Id_Android_Interticial = "";
+
+    [Header("Datos Iphone")]
+    public string Id_iOS_APP = "";
     public string Id_Ios_Bonificado = "";
     public string Id_Ios_Interticial = "";
-  
-
-    public string Id_Android_APP = "";
-    public string Id_iOS_APP = "";
 
 
+    [Header("Test")]
     public string Id_TEST_Bonificado = "";
     public string Id_TEST_Interticial = "";
+
+
+
+
+    private void Awake()
+    {
+        ID_App = MyPlataforma(Id_Android_APP, Id_iOS_APP);
+
+        ID_VideoReward = MyPlataforma(Id_Android_Bonificado, Id_TEST_Bonificado);
+        ID_Intersticial = MyPlataforma(Id_Android_Interticial, Id_Ios_Interticial);
+
+        if(testing)
+        {
+            ID_VideoReward = Id_TEST_Bonificado;
+            ID_Intersticial = Id_TEST_Interticial;
+        }
+
+    }
+
+
+    public string MyPlataforma(string Android, string Apple)
+    {
+    #if UNITY_ANDROID
+    return Android;
+#elif UNITY_IOS
+    return Apple;
+#else
+return Android;
+#endif
+    }
+
 
 }
