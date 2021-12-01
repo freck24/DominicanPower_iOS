8a969d622ffeb790fec8e8662d46424b4f03adcc
Frederick Ventura <f.ventura.l@hotmail.com>
2021-11-30 22:39:36 -0400

    menu update

---
+++ DOMINICAN GAME/Assets/GESTIONDEDESPLEGABLE.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GESTIONDEDESPLEGABLE : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator anim;
+    public GameObject botonsubuir;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void sube()
+    {
+        anim.SetBool("SUBE", true);
+        botonsubuir.SetActive(false);
+    }
+    public void subefALSE()
+    {
+        anim.SetBool("SUBE", false);
+      
+    }
+       public void activar()
+    {
+        
+        botonsubuir.SetActive(true);
+    }
+
+}
