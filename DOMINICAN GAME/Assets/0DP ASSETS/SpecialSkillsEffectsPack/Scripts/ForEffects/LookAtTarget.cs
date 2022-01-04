72f1531954f110978488412f1ba6984d22ccdc65
Frederick Ventura <f.ventura.l@hotmail.com>
2022-01-04 09:41:30 -0400

    add FX

--- DOMINICAN GAME/Assets/0DP ASSETS/SpecialSkillsEffectsPack/Scripts/ForEffects/LookAtTarget.cs
+++ DOMINICAN GAME/Assets/0DP ASSETS/SpecialSkillsEffectsPack/Scripts/ForEffects/LookAtTarget.cs
@@ -5,12 +5,10 @@ using UnityEngine;
 public class LookAtTarget : MonoBehaviour
 {
     public Transform Target;
-    public int x = 1;
-    public int y = 1;
-    public int z = 1;
+
     // Update is called once per frame
     void Update()
     {
-        transform.LookAt(new Vector3(Target.position.x*x, Target.position.y * y, Target.position.x * z));
+        transform.LookAt(Target);
     }
 }
